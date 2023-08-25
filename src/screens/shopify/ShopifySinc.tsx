23d42704264fd6276739eb5a3050994c4b52983c
Alexxsantiag0 <alexander.santiago.cg@gmail.com>
2023-08-25 04:14:07 -0600

    aaaa
    aaaa

--- src/screens/shopify/ShopifySinc.tsx
+++ src/screens/shopify/ShopifySinc.tsx
@@ -63,33 +63,154 @@ function ShopifySinc() {
     }
   };
 
-  const fetchClientes = async () => {
-    try {
-      const response = await axios.get("http://localhost:3001/api/clientes");
+  // const fetchClientes = async () => {
+  //   try {
+  //     const response = await axios.get("http://localhost:3001/api/clientes");
 
+  //     if (response.data) {
+  //       setClientJson(response.data);
+  //       setError(null);
+  //       const jsonString = transformToJsonString(response.data);
+  //       setJsonString(jsonString); // Actualiza el estado con la cadena JSON transformada
+  //       await sendClientes(response.data);
+  //     } else {
+  //       setError("Los datos de órdenes no son válidos.");
+  //     }
+  //   } catch (error) {
+  //     console.error("Error al obtener las órdenes:", error);
+  //     setError("Ocurrió un error al obtener las órdenes.");
+  //   }
+  // };
+
+  
+  // const fetchClientes = async (pageInfo = '') => {
+  //   try {
+  //     // Realiza la solicitud GET al servidor utilizando el valor de page_info
+  //     const response = await axios.get(`http://localhost:3001/api/clientes?page_info=${pageInfo}`);
+      
+  //     if (response.data) {
+  //       setClientJson(response.data);
+  //       setError(null);
+  //       const jsonString = transformToJsonString(response.data);
+  //       setJsonString(jsonString); // Actualiza el estado con la cadena JSON transformada
+  //       await sendClientes(response.data);
+        
+  //       // Verifica si hay un enlace a la siguiente página en el encabezado link
+  //       const linkHeader = response.headers.link;
+  //       const nextLink = linkHeader && linkHeader.match(/<([^>]+)>;\s*rel="next"/);
+  
+  //       if (nextLink) {
+  //         const nextUrl = nextLink[1];
+  //         // Extrae el valor de page_info de la URL para la próxima página
+  //         const nextPageInfo = nextUrl.match(/page_info=([^&]+)/)[1];
+  
+  //         // Espera un segundo antes de hacer la siguiente solicitud
+  //         setTimeout(() => {
+  //           fetchClientes(nextPageInfo);
+  //         }, 1000);
+  //       } else {
+  //         // Todos los clientes se han sincronizado
+  //         Swal.fire({
+  //           icon: "success",
+  //           text: "Sincronización exitosa",
+  //           confirmButtonColor: "#3085d6",
+  //         });
+  //       }
+  //     } else {
+  //       setError("Los datos de clientes no son válidos.");
+  //     }
+  //   } catch (error) {
+  //     console.error("Error al obtener los clientes:", error);
+  //     setError("Ocurrió un error al obtener los clientes.");
+  //   }
+  // };
+  
+  
+  
+  
+  
+  // const sendClientes = async (clientData) => {
+  //   try {
+  //     // Convierte el objeto JSON a una cadena y escapa las comillas dobles
+  //     const jsonString = JSON.stringify(clientData).replace(/"/g, '\\"');
+
+  //     // Envuelve la cadena JSON entre comillas
+  //     const wrappedJsonString = `"${jsonString}"`;
+
+  //     // Realiza la solicitud POST con la cadena JSON envuelta en comillas como cuerpo
+  //     await jezaApi
+  //       .post("/ShopifyClientes", wrappedJsonString, {
+  //         headers: {
+  //           "Content-Type": "application/json", // Indica que el cuerpo es texto plano
+  //         },
+  //       })
+  //       .then((response) => {
+  //         Swal.fire({
+  //           icon: "success",
+  //           text: "Sincronización exitosa",
+  //           confirmButtonColor: "#3085d6",
+  //         });
+  //       })
+  //       .catch((error) => {
+  //         console.log(error);
+  //       });
+  //   } catch (error) {
+  //     console.error("Error al enviar las órdenes a Jeza:", error);
+  //     // Manejar errores aquí
+  //   }
+  // };
+
+  const fetchClientes = async (pageInfo = '') => {
+    try {
+      // Realiza la solicitud GET al servidor utilizando el valor de page_info
+      const response = await axios.get(`http://localhost:3001/api/clientes?page_info=${pageInfo}`);
+      
       if (response.data) {
         setClientJson(response.data);
         setError(null);
         const jsonString = transformToJsonString(response.data);
         setJsonString(jsonString); // Actualiza el estado con la cadena JSON transformada
         await sendClientes(response.data);
+  
+        // Verifica si hay un enlace a la siguiente página en el encabezado link
+        const linkHeader = response.headers.link;
+        const nextLink = linkHeader && linkHeader.match(/<([^>]+)>;\s*rel="next"/);
+  
+        if (nextLink) {
+          const nextUrl = nextLink[1];
+          // Extrae el valor de page_info de la URL para la próxima página
+          const nextPageInfo = nextUrl.match(/page_info=([^&]+)/)[1];
+  
+          // Espera un segundo antes de hacer la siguiente solicitud
+          setTimeout(() => {
+            fetchClientes(nextPageInfo);
+          }, 1000);
+        } else {
+          // Todos los clientes se han sincronizado
+          // Swal.fire({
+          //   icon: "success",
+          //   text: "Sincronización exitosa",
+          //   confirmButtonColor: "#3085d6",
+          // });
+        }
       } else {
-        setError("Los datos de órdenes no son válidos.");
+        setError("Los datos de clientes no son válidos.");
       }
     } catch (error) {
-      console.error("Error al obtener las órdenes:", error);
-      setError("Ocurrió un error al obtener las órdenes.");
+      console.error("Error al obtener los clientes:", error);
+      setError("Ocurrió un error al obtener los clientes.");
     }
   };
-
+  
+  
   const sendClientes = async (clientData) => {
     try {
       // Convierte el objeto JSON a una cadena y escapa las comillas dobles
       const jsonString = JSON.stringify(clientData).replace(/"/g, '\\"');
-
+  
       // Envuelve la cadena JSON entre comillas
       const wrappedJsonString = `"${jsonString}"`;
-
+  
       // Realiza la solicitud POST con la cadena JSON envuelta en comillas como cuerpo
       await jezaApi
         .post("/ShopifyClientes", wrappedJsonString, {
@@ -98,11 +219,11 @@ function ShopifySinc() {
           },
         })
         .then((response) => {
-          Swal.fire({
-            icon: "success",
-            text: "Sincronización exitosa",
-            confirmButtonColor: "#3085d6",
-          });
+          // Swal.fire({
+          //   icon: "success",
+          //   text: "Sincronización exitosa",
+          //   confirmButtonColor: "#3085d6",
+          // });
         })
         .catch((error) => {
           console.log(error);
@@ -112,6 +233,7 @@ function ShopifySinc() {
       // Manejar errores aquí
     }
   };
+  
 
   const fetchProduct = async () => {
     try {
@@ -184,9 +306,10 @@ function ShopifySinc() {
           <Button color="primary" onClick={fetchOrders}>
             Órdenes
           </Button>
-          <Button color="primary" onClick={fetchClientes}>
-            Clientes
-          </Button>
+          <Button color="primary" onClick={() => fetchClientes('')}>
+  Clientes
+</Button>
+
           <Button color="primary" disabled="true" onClick={fetchProduct}>
             Productos...en construccion
           </Button>
