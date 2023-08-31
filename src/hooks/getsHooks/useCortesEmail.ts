820d2402062d1c56e0f46413d8bb264baf31bca7
LuisSG9915 <luis.sg9915@gmail.com>
2023-08-31 12:51:13 -0600

    Corrección de error fechas CORTES

--- src/hooks/getsHooks/useCortesEmail.ts
+++ src/hooks/getsHooks/useCortesEmail.ts
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { AxiosResponse } from "axios";
 import { jezaApi } from "../../api/jezaApi";
 import { CorteA, CorteB, CorteC } from "../../models/CortesEmail";
+import { format } from "date-fns";
+
 interface Props {
   sucursal: number;
   fecha: Date;
@@ -14,9 +16,13 @@ export const useCortesEmail = ({ sucursal, fecha }: Props) => {
   const [ColumnasA, setColumnasA] = useState([]);
   const [ColumnasB, setColumnasB] = useState([]);
   const [ColumnasC, setColumnasC] = useState([]);
+  const currentDate = new Date();
+
   const fetchCorteA = async () => {
     try {
-      const response: AxiosResponse<any[]> = await jezaApi.get(`/CorteSeccionA?suc=${sucursal}&fecha=${fecha ? fecha : new Date()}`);
+      const response: AxiosResponse<any[]> = await jezaApi.get(
+        `/CorteSeccionA?suc=${sucursal}&fecha=${fecha ? fecha : format(currentDate, "yyyy-MM-dd")}`
+      );
       setDataCorteEmailA(response.data);
     } catch (error) {
       console.log(error);
@@ -24,7 +30,9 @@ export const useCortesEmail = ({ sucursal, fecha }: Props) => {
   };
   const fetchCorteB = async () => {
     try {
-      const response: AxiosResponse<any[]> = await jezaApi.get(`/CorteSeccionB?suc=${sucursal}&fecha=${fecha ? fecha : new Date()}`);
+      const response: AxiosResponse<any[]> = await jezaApi.get(
+        `/CorteSeccionB?suc=${sucursal}&fecha=${fecha ? fecha : format(currentDate, "yyyy-MM-dd")}`
+      );
       setDataCorteEmailB(response.data);
       if (response.data.length > 0) {
         const columnKeys = Object.keys(response.data[0]);
@@ -41,7 +49,9 @@ export const useCortesEmail = ({ sucursal, fecha }: Props) => {
   };
   const fetchCorteC = async () => {
     try {
-      const response: AxiosResponse<any[]> = await jezaApi.get(`/CorteSeccionC?suc=${sucursal}&fecha=${fecha ? fecha : new Date()}`);
+      const response: AxiosResponse<any[]> = await jezaApi.get(
+        `/CorteSeccionC?suc=${sucursal}&fecha=${fecha ? fecha : format(currentDate, "yyyy-MM-dd")}`
+      );
       setDataCorteEmailC(response.data);
       if (response.data.length > 0) {
         const columnKeys = Object.keys(response.data[0]);
