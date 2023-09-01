97c8665f3aaeda1bb36e7956a7547d175eee906c
alexxSantiag0 <alexander.santiago.cg@gmail.com>
2023-09-01 10:29:15 -0600

    lector de codigo de barras ventas

--- src/hooks/getsHooks/useInsumosGenerales.ts
+++ src/hooks/getsHooks/useInsumosGenerales.ts
@@ -7,11 +7,19 @@ interface Props {
 }
 
 export const useInsumosGenerales = ({ marca }: Props) => {
+
+ 
   const [dataInsumoGenerales, setDataInsumosGenerales] = useState<Venta[]>([]);
   const fetchInsumosGenerales = async () => {
     try {
+      if(marca){
+         const response: AxiosResponse<any[]> = await jezaApi.get(`/ProductoInsumo?id=0&palabra=${marca}`);
+      setDataInsumosGenerales(response.data);
+      }else{
       const response: AxiosResponse<any[]> = await jezaApi.get(`/ProductoInsumo?id=0&palabra=%`);
       setDataInsumosGenerales(response.data);
+      
+      }
     } catch (error) {
       console.log(error);
     }
