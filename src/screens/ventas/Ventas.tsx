76a271fb70cd0e07a83012cb2c0ef45a009d6399
LuisSG9915 <luis.sg9915@gmail.com>
2023-10-12 19:22:42 -0600

    Update Ventas.tsx

--- src/screens/ventas/Ventas.tsx
+++ src/screens/ventas/Ventas.tsx
@@ -1711,7 +1711,11 @@ const Ventas = () => {
                   icon: "info",
                   text: "No ha seleccionado estilista",
                 });
-              } else if (dataTemporal?.d_existencia < dataTemporal.Cant_producto && dataTemporal.Observacion !== "SERV") {
+              } else if (
+                dataTemporal?.d_existencia &&
+                dataTemporal?.d_existencia < dataTemporal.Cant_producto &&
+                dataTemporal.Observacion !== "SERV"
+              ) {
                 Swal.fire({
                   icon: "info",
                   text: "No hay existencias ",
