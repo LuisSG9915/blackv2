5a5633ae2eb927dcf490c2e198c875109e16fd09
LuisSG9915 <luis.sg9915@gmail.com>
2023-12-09 10:35:44 -0600

    ventas descuentos

--- src/screens/EXAMPLE/Example2.tsx
+++ src/screens/EXAMPLE/Example2.tsx
@@ -9,17 +9,51 @@ function Example2() {
     username: "",
     password: "",
   });
+
+  // axios.interceptors.request.use(
+  //   (config) => {
+  //     // Obtiene el token almacenado (asegúrate de haberlo almacenado previamente)
+  //     const token = testToken; // Implementa tu lógica para obtener el token
+
+  //     // Agrega el encabezado de autorización si hay un token
+  //     if (token) {
+  //       config.headers.Authorization = `${token}`;
+  //       alert("a");
+  //     }
+
+  //     return config;
+  //   },
+  //   (error) => {
+  //     return Promise.reject(error);
+  //   }
+  // );
+
+  const [testToken, setTestToken] = useState("");
   const handleChange = (e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement>) => {
     const { name, value } = e.target;
     setPruebaApi((prevState: any) => ({ ...prevState, [name]: value }));
   };
   const apiExample = () => {
-    axios.post("http://127.0.0.1:3003/login/prueba", pruebaApi);
+    axios.post("http://127.0.0.1:3003/login/prueba", pruebaApi).then((response) => {
+      setTestToken(response.data.token);
+    });
+  };
+  const apiPruebas = () => {
+    axios
+      .get("http://127.0.0.1:3003/testToken", {
+        headers: {
+          Authorization: testToken,
+        },
+      })
+      .then((response) => {
+        console.log(response.data.msg);
+      });
   };
 
   return (
     <div>
       <Button onClick={() => apiExample()}>Boton prueba</Button>
+      <Button onClick={() => apiPruebas()}>Botón verificación</Button>
       <Col>
         <Row>
           <Input name="username" onChange={handleChange}></Input>
--- src/screens/ventas/Ventas.tsx
+++ src/screens/ventas/Ventas.tsx
@@ -508,7 +508,7 @@ const Ventas = () => {
   // SUMATORIAS
   useEffect(() => {
     const totalCobro = Number(formPago.anticipos) + Number(formPago.efectivo) + Number(formPago.tc);
-    const totalCambio = Number(totalCobro) - Number(total);
+    const totalCambio = Number(totalCobro.toLocaleString(2)) - Number(total.toLocaleString(2));
     setFormPago((prev) => ({
       ...prev,
       totalPago: totalCobro,
@@ -681,7 +681,7 @@ const Ventas = () => {
               });
               fetchVentas();
             });
-        } catch (error) { }
+        } catch (error) {}
       }
     });
   };
@@ -807,7 +807,7 @@ const Ventas = () => {
       totalPorProductoNormal += producto.Precio * producto.Cant_producto - producto.Precio * producto.Cant_producto * producto.Descuento;
     });
 
-    setTotal(Number(totalPorProductoNormal));
+    setTotal(Number(totalPorProductoNormal.toLocaleString(2)));
     setTiempo(dataVentas.reduce((total, objeto) => total + (objeto.tiempo ?? 0), 0));
   }, [dataVentas]);
 
@@ -941,7 +941,8 @@ const Ventas = () => {
         const temp = response.data.mensaje2;
         jezaApi
           .get(
-            `/TicketVta?folio=${Number(response.data.mensaje2)}&caja=1&suc=${Number(dataUsuarios2[0]?.sucursal)}&usr=${dataUsuarios2[0]?.id
+            `/TicketVta?folio=${Number(response.data.mensaje2)}&caja=1&suc=${Number(dataUsuarios2[0]?.sucursal)}&usr=${
+              dataUsuarios2[0]?.id
             }&pago=${Number(formPago.totalPago)}`
           )
           .then((response) => {
@@ -1170,8 +1171,8 @@ const Ventas = () => {
             Number(elemento.formaPago) === 94 || Number(elemento.formaPago) === 245
               ? anticipoIdentificador
               : elemento.referencia
-                ? elemento.referencia
-                : "Efectivo",
+              ? elemento.referencia
+              : "Efectivo",
           importe: elemento.formaPago == 1 ? elemento.importe - formPago.cambioCliente : elemento.importe,
           usuario: dataUsuarios2[0]?.id,
         },
@@ -1213,12 +1214,18 @@ const Ventas = () => {
 
     jezaApi
       .put(
-        `/Venta?id=${dataVentaEdit.id}&Cia=${dataUsuarios2[0]?.idCia}&Sucursal=${dataUsuarios2[0]?.sucursal
-        }&Fecha=${formattedDate}&Caja=1&No_venta=0&no_venta2=0&Clave_prod=${dataVentaEdit.Clave_prod}&Cant_producto=${dataVentaEdit.Cant_producto
-        }&Precio=${dataVentaEdit.Precio}&Cve_cliente=${dataVentaEdit.Cve_cliente}&Tasa_iva=0.16&Observacion=${dataVentaEdit.Observacion}&Descuento=${dataVentaEdit.Descuento
-        }&Clave_Descuento=${dataVentaEdit.Clave_Descuento}&usuario=${dataVentaEdit.idEstilista}&Corte=1&Corte_parcial=1&Costo=${dataVentaEdit.Costo
-        }&Precio_base=${dataVentaEdit.Precio_base}&No_venta_original=0&cancelada=false&folio_estilista=${0}&hora=${horaFormateada}&tiempo=${dataVentaEdit.tiempo === 0 ? 0 : dataVentaEdit.tiempo
-        }&terminado=false&validadoServicio=false&idestilistaAux=${dataVentaEdit.idestilistaAux ? dataVentaEdit.idestilistaAux : 0}&idRecepcionista=${dataUsuarios2[0]?.id
+        `/Venta?id=${dataVentaEdit.id}&Cia=${dataUsuarios2[0]?.idCia}&Sucursal=${
+          dataUsuarios2[0]?.sucursal
+        }&Fecha=${formattedDate}&Caja=1&No_venta=0&no_venta2=0&Clave_prod=${dataVentaEdit.Clave_prod}&Cant_producto=${
+          dataVentaEdit.Cant_producto
+        }&Precio=${dataVentaEdit.Precio}&Cve_cliente=${dataVentaEdit.Cve_cliente}&Tasa_iva=0.16&Observacion=${dataVentaEdit.Observacion}&Descuento=${
+          dataVentaEdit.Descuento
+        }&Clave_Descuento=${dataVentaEdit.Clave_Descuento}&usuario=${dataVentaEdit.idEstilista}&Corte=1&Corte_parcial=1&Costo=${
+          dataVentaEdit.Costo
+        }&Precio_base=${dataVentaEdit.Precio_base}&No_venta_original=0&cancelada=false&folio_estilista=${0}&hora=${horaFormateada}&tiempo=${
+          dataVentaEdit.tiempo === 0 ? 0 : dataVentaEdit.tiempo
+        }&terminado=false&validadoServicio=false&idestilistaAux=${dataVentaEdit.idestilistaAux ? dataVentaEdit.idestilistaAux : 0}&idRecepcionista=${
+          dataUsuarios2[0]?.id
         }`
       )
       .then(() => {
@@ -1600,18 +1607,18 @@ const Ventas = () => {
                     <td>
                       {dato.Descuento === 0
                         ? (dato.Precio * dato.Cant_producto).toLocaleString("es-MX", {
-                          style: "currency",
-                          currency: "MXN", // Código de moneda para el Peso Mexicano
-                          minimumFractionDigits: 2,
-                          maximumFractionDigits: 2,
-                        })
+                            style: "currency",
+                            currency: "MXN", // Código de moneda para el Peso Mexicano
+                            minimumFractionDigits: 2,
+                            maximumFractionDigits: 2,
+                          })
                         : (dato.Precio * dato.Cant_producto - dato.Precio * dato.Cant_producto * dato.Descuento).toLocaleString("es-MX", {
-                          style: "currency",
-                          currency: "MXN", // Código de moneda para el Peso Mexicano
+                            style: "currency",
+                            currency: "MXN", // Código de moneda para el Peso Mexicano
 
-                          minimumFractionDigits: 2,
-                          maximumFractionDigits: 2,
-                        })}
+                            minimumFractionDigits: 2,
+                            maximumFractionDigits: 2,
+                          })}
                     </td>
                     <td className="gap-5">
                       <AiFillDelete
@@ -2272,47 +2279,47 @@ const Ventas = () => {
             <tbody>
               {datoInsumosProductoResumen.length > 0
                 ? datoInsumosProductoResumen.map((dato: any) => (
-                  <tr key={dato.id}>
-                    {dato.id ? (
-                      <>
-                        <td>{dato.d_insumo}</td>
-                        <td align="left">{cantidadInsumo(dato.id_insumo)}</td>
-                        <td align="left">{dato.unidadMedida}</td>
-                        <td align="center">{dato.cantidad}</td>
+                    <tr key={dato.id}>
+                      {dato.id ? (
+                        <>
+                          <td>{dato.d_insumo}</td>
+                          <td align="left">{cantidadInsumo(dato.id_insumo)}</td>
+                          <td align="left">{dato.unidadMedida}</td>
+                          <td align="center">{dato.cantidad}</td>
 
-                        <td className="gap-5">
-                          <AiFillEdit
-                            className="mr-2"
-                            onClick={() => {
-                              setModalEditInsumo(true);
-                              setFormInsumo({
-                                cantidad: dato.cantidad,
-                                fechaAlta: dato.fechaAlta,
-                                id: dato.id,
-                                id_insumo: dato.id_insumo,
-                                id_venta: dato.id_venta,
-                                unidadMedida: dato.unidadMedida,
-                                d_insumo: dato.d_insumo,
-                                existencia: cantidadInsumo(dato.id_insumo),
-                              });
-                            }}
-                            size={23}
-                          ></AiFillEdit>
-                          <AiFillDelete
-                            color="lightred"
-                            onClick={() => {
-                              deleteInsumo(dato);
-                              setTimeout(() => {
-                                fetchInsumosProducto();
-                              }, 1000);
-                            }}
-                            size={23}
-                          />
-                        </td>
-                      </>
-                    ) : null}
-                  </tr>
-                ))
+                          <td className="gap-5">
+                            <AiFillEdit
+                              className="mr-2"
+                              onClick={() => {
+                                setModalEditInsumo(true);
+                                setFormInsumo({
+                                  cantidad: dato.cantidad,
+                                  fechaAlta: dato.fechaAlta,
+                                  id: dato.id,
+                                  id_insumo: dato.id_insumo,
+                                  id_venta: dato.id_venta,
+                                  unidadMedida: dato.unidadMedida,
+                                  d_insumo: dato.d_insumo,
+                                  existencia: cantidadInsumo(dato.id_insumo),
+                                });
+                              }}
+                              size={23}
+                            ></AiFillEdit>
+                            <AiFillDelete
+                              color="lightred"
+                              onClick={() => {
+                                deleteInsumo(dato);
+                                setTimeout(() => {
+                                  fetchInsumosProducto();
+                                }, 1000);
+                              }}
+                              size={23}
+                            />
+                          </td>
+                        </>
+                      ) : null}
+                    </tr>
+                  ))
                 : null}
             </tbody>
           </Table>
@@ -2609,13 +2616,13 @@ const Ventas = () => {
           ></Input>
           <br />
           {dataArregloTemporal.formaPago == 90 ||
-            dataArregloTemporal.formaPago == 91 ||
-            dataArregloTemporal.formaPago == 80 ||
-            dataArregloTemporal.formaPago == 92 ||
-            dataArregloTemporal.formaPago == 100 ||
-            dataArregloTemporal.formaPago == 101 ||
-            dataArregloTemporal.formaPago == 110 ||
-            dataArregloTemporal.formaPago == 103 ? (
+          dataArregloTemporal.formaPago == 91 ||
+          dataArregloTemporal.formaPago == 80 ||
+          dataArregloTemporal.formaPago == 92 ||
+          dataArregloTemporal.formaPago == 100 ||
+          dataArregloTemporal.formaPago == 101 ||
+          dataArregloTemporal.formaPago == 110 ||
+          dataArregloTemporal.formaPago == 103 ? (
             <>
               <Label> Referencia: </Label>
               <Input onChange={handleFormaPagoTemporal} value={dataArregloTemporal.referencia} name={"referencia"}></Input>
